1aa5fa59e99efbf52694c546513aafafee20b717
wangpeng <993080086@qq.com>
2022-03-31 00:29:25 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import uView from 'uview-ui'
+import App from './App'
+
+Vue.use(uView)
+Vue.config.productionTip = false
+App.mpType = 'app'
+
+const app = new Vue({
+  ...App
+})
+
+app.$mount()
